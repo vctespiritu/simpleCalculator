a1d19230a78b2141851b8f57e69e4007c67a5654
Victor Espiritu <vicespi_1123@hotmail.es>
2024-03-30 00:22:11 -0400

    Final

--- force-app/main/default/lwc/simpleCalculator/simpleCalculator.js
+++ force-app/main/default/lwc/simpleCalculator/simpleCalculator.js
@@ -1,17 +1,99 @@
-import { LightningElement, track } from 'lwc';
+import { LightningElement } from 'lwc';
 
-export default class SimpleCalculator extends LightningElement {
+export default class CalcFull extends LightningElement {
 
-    @track calcExpressionFieldValue = '';
+    calcResult = '';
+    calcExpression = '';
+    clrExpression = false;
+    prevOper = '';
+    operations = {
+        current: 0,
+        '=': function(){
+            return this.current;
+        },
+        '+': function(n){
+            this.current += parseInt(n);
+            return this;
+        },
+        '-': function(n){
+            this.current -= parseInt(n);
+            return this;
+        },
+        '*': function(n){
+            this.current *= parseInt(n);
+            return this;
+        },
+        '/': function(n){
+            this.current /= parseInt(n);
+            return this;
+        }
+    }
 
-    handleClick(e){
+    get showResult(){
+        return this.operations.current;
+    }
 
-        if(!(e.target.name === 'CLR' || e.target.name === '=')){
-            this.calcExpressionFieldValue += e.target.name;
-        }else if(e.target.name === 'CLR'){
-            this.calcExpressionFieldValue = '';
-        }else if(e.target.name === '='){
-            // TODO
+    handleClick(event){
+
+        if (this.clrExpression){
+            this.calcExpression = '';
+            this.calcResult = '';
+            this.operations.current = 0;
+            this.clrExpression = false;
+        }
+
+        this.calcExpression = this.calcExpression + event.target.label;
+
+        if (event.target.label === "CLR"){
+            this.calcResult = '';
+            this.calcExpression = '';
+            this.operations.current = 0;
+        }
+        else if (event.target.label === "+"){
+            if (this.operations.current === 0){
+                this.operations.current = parseInt(this.calcResult);
+            }else{
+                this.calcResult = this.operations[this.prevOper](this.calcResult);
+            }
+            this.prevOper = '+';
+            this.calcResult = '';
+        }
+        else if (event.target.label === "-"){
+            if (this.operations.current === 0){
+                this.operations.current = parseInt(this.calcResult);
+            }else{
+                this.calcResult = this.operations[this.prevOper](this.calcResult);
+            }
+            this.prevOper = '-';
+            this.calcResult = '';
+        }
+        else if (event.target.label === "*"){
+            if (this.operations.current === 0){
+                this.operations.current = parseInt(this.calcResult);
+            }else{
+                this.calcResult = this.operations[this.prevOper](this.calcResult);
+            }
+            this.prevOper = '*';
+            this.calcResult = '';
+        }
+        else if (event.target.label === "/"){
+            if (this.operations.current === 0)
+            {
+                this.operations.current = parseInt(this.calcResult);
+            }
+            else{
+                this.calcResult = this.operations[this.prevOper](this.calcResult);
+            }
+            this.prevOper = '/';
+            this.calcResult = '';
+        }
+        else if (event.target.label === "="){
+            this.calcResult = this.operations[this.prevOper](this.calcResult);
+            this.calcResult = this.operations['=']();
+            this.clrExpression = true;
+        }
+        else{
+            this.calcResult = this.calcResult + event.target.label;
         }
     }
 }
